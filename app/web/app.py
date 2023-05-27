980ad902ab7166919fe01f0b173b42daa28c811c
Astra <git@astra.lol>
2023-05-27 02:24:29 -0700

    Use db.json from mount

--- app/web/app.py
+++ app/web/app.py
@@ -2,15 +2,17 @@ from typing import Union
 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
 @app.get("/db.json")
-async def read_root():
-    return {}
+async def db():
+    return FileResponse("mount/db.json")
 
-# These must go last
-app.mount("/sounds", StaticFiles(directory="app/web/static"), name="static")
+# These must go after routes
+
+app.mount("/sounds", StaticFiles(directory="mount/sounds"), name="static")
 app.mount("/", StaticFiles(directory="app/web/static", html=True), name="static")
 
 
