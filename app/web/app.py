9a6a4712537e59df7a5cc8209666bb5763c69bcd
clairitin <claire@thewired.io>
2023-05-29 14:37:06 -0700

    add sorting, better mobile styles

--- app/web/app.py
+++ app/web/app.py
@@ -1,19 +1,32 @@
-from typing import Union
+import json
+import pathlib
+
+from typing import Any, Dict, Union
 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 
+from app.web.models import DB
+
 app = FastAPI()
 
 @app.get("/db.json")
 async def db():
-    return FileResponse("mount/db.json")
+    with open("mount/db.json") as db_file:
+        db_json: Dict[str, Any] = json.load(db_file)
+
+    db = DB.parse_obj(db_json)
+    
+    for sound in db.sounds:
+        sound_files = list(pathlib.Path("mount/sounds/").glob(f"{sound.name}.*"))
+        if len(sound_files) > 0:
+            sound.filename = sound_files[0].name
+            sound.modified = sound_files[0].stat().st_mtime_ns
+
+    return db
 
 # These must go after routes
 
 app.mount("/sounds", StaticFiles(directory="mount/sounds"), name="static")
 app.mount("/", StaticFiles(directory="app/web/static", html=True), name="static")
-
-
-    
---
+++ app/web/models.py
@@ -0,0 +1,15 @@
+from typing import Dict, Optional, List
+from pydantic import BaseModel
+
+class Sound(BaseModel):
+    name: str
+    filename: Optional[str]
+    modified: Optional[int]
+    count: int
+    tags: List[str]
+
+class DB(BaseModel):
+    entrances: Dict[str,str]
+    exits: Dict[str,str]
+    sounds: List[Sound]
+    ignoreList: List[str]
